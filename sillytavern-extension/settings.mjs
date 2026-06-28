3ace4ca728280404e629d0e5b625c94ee857821a
lolo <qrewq5003@gmail.com>
2026-06-28 04:08:30 +0300

    fix: update ST extension default URL to port 8001

--- sillytavern-extension/settings.mjs
+++ sillytavern-extension/settings.mjs
@@ -10,7 +10,7 @@ import {
 
 export const DEFAULT_CONNECTION_SETTINGS = {
     enabled: false,
-    memoryServiceUrl: 'http://localhost:8000',
+    memoryServiceUrl: 'http://localhost:8001',
     apiKey: '',
 };
 
